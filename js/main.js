08eb94013bb591f3d8857c25236e7b7d93df292b
petronellabodzay <97516324+petronellabodzay@users.noreply.github.com>
2022-02-06 13:36:41 +0100

    ok

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    var myContent = document.getElementById('content');
+
+    myContent.innerHTML = '<h3>Testar att skriva en text här som genereras via Javascript</h3>';
+
+    window.console.log('Sandbox is ready!');
+})();
